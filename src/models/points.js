8b82a4524c85cd923dfae42a8f3b3fe7803775c3
Maria <reflection2008@gmail.com>
2020-01-27 09:40:03 +0300

    master

--- src/models/points.js
+++ src/models/points.js
@@ -40,7 +40,6 @@ export default class Points {
     if (index === -1) {
       return false;
     }
-  //  this._applyFlatpickr();
     this._points = [].concat(this._points.slice(0, index), newPoint, this._points.slice(index + 1));
     this._callHandlers(this._dataChangeHandlers);
     return true;
